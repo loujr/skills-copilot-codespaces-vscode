686f48550b20f7a9f2b38e46a2090ea71f1c30e5
Lou Nelson Jr. <61295275+loujr@users.noreply.github.com>
2023-12-04 19:28:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,64 @@
 // Create Web server
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// Load Comments Model
-var Comments = require('../models/comments');
+// Import comment model
+const Comment = require('../models/comment');
 
-// Get all comments
-router.get('/', function(req, res) {
-    Comments.find({}, function(err, comments) {
-        if (err) {
-            console.log(err);
-        } else {
+// Import image model
+const Image = require('../models/image');
+
+// Import user model
+const User = require('../models/user');
+
+// Import passport
+const passport = require('passport');
+
+// Import authentication
+const authentication = require('../authentication');
+
+// Import multer
+const multer = require('multer');
+
+// Import cloudinary
+const cloudinary = require('cloudinary');
+
+// Import cloudinary storage
+const storage = require('../cloudinaryStorage');
+
+// Configure multer
+const upload = multer({ storage: storage });
+
+// Configure cloudinary
+cloudinary.config({
+    cloud_name: 'dofk2w5xg',
+    api_key: '725978634558481',
+    api_secret: 'jwUvKXHk2Bx8N7Xj3n4cUJnWfJk'
+});
+
+// GET request to get all comments
+// http://localhost:5000/comments
+router.get('/', (req, res, next) => {
+    Comment.find({})
+        .then((comments) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
             res.json(comments);
-        }
-    });
+        }, (err) => next(err))
+        .catch((err) => next(err));
 });
 
-// Get one comment
-router.get('/:id', function(req, res) {
-    Comments.findById(req.params.id, function(err, comment) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(comment);
-        }
-    });
-});
+// POST request to add a comment
+// http://localhost:5000/comments
+router.post('/', authentication.verifyUser, (req, res, next) => {
+    // Get the image id from the request body
+    const imageId = req.body.imageId;
 
-// Create new comment
-router.post('/', function(req, res) {
-    var comment = new Comments({
-        title: req.body.title,
-        content: req.body.content,
+    // Get the comment from the request body
+    const comment = req.body.comment;
+
+    // Create a new comment object
+    const newComment = {
+        imageId: imageId,
+        comment: comment,
