735e7e7ac7561874556938116f6fb4ab7a42ce19
Lou Nelson Jr. <61295275+loujr@users.noreply.github.com>
2023-12-04 19:02:58 +0000

    added member js

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '=',
+            index: '='
+        }
+    }
+}
